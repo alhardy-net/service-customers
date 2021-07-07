71b80bf71f1fa0508cc0ee3c546ddfd231953a1f
Allan Hardy <al_hardy@live.com.au>
2021-07-07 21:13:01 +1000

    initial commit

---
+++ src/Customers.Api/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Customers.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ILogger<CustomersController> _logger;
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public Task<List<Customer>> Get(int id)
+        {
+            return Task.FromResult(new List<Customer>
+                { new() { FirstName = "Allan", Id = 1, LastName = "Hardy" } });
+        }
+
+        [HttpPost]
+        public async Task Post(Customer customer)
+        {
+            await Task.CompletedTask;
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Task.CompletedTask;
+
+            return Ok();
+        }
+    }
+}
