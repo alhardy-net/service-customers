6539276b86cd319ac405c4164138d5666aebcbc4
Allan Hardy <al_hardy@live.com.au>
2021-07-12 20:03:43 +1000

    test update

--- src/Customers.Api/Controllers/MetaController.cs
+++ src/Customers.Api/Controllers/MetaController.cs
@@ -10,7 +10,7 @@ namespace Customers.Api.Controllers
         [HttpGet("ping")]
         public ActionResult<string> Ping()
         {
-            return Ok("pong");
+            return Ok("pong2");
         }
     }
 }
