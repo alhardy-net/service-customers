9f11209d674aaa90b4a49246ad5757205f338290
Allan Hardy <al_hardy@live.com.au>
2021-08-04 22:30:37 +1000

    adding temp endpoint to test secrets

--- src/Customers.Api/Controllers/MetaController.cs
+++ src/Customers.Api/Controllers/MetaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Customers.Api.Controllers
 {
@@ -7,10 +8,23 @@ namespace Customers.Api.Controllers
     [ApiController]
     public class MetaController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public MetaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("ping")]
         public ActionResult<string> Ping()
         {
             return Ok("pong");
+        }
+
+        [HttpGet("secret")]
+        public ActionResult<string> ShowSecret()
+        {
+            return Ok(_configuration.GetValue<string>("customers/customer-api/supersecret:supersecret"));
         }
     }
 }
